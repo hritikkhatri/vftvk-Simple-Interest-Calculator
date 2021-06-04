ccab8043788426e9ee97cc6029d2b942666bfa88
hritikkhatri <40753903+hritikkhatri@users.noreply.github.com>
2021-06-05 02:24:52 +0530

    final

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
-    
+{   if(validateAmount())
+    { 
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "%</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + year + "</mark>";
 }
-        
+}
+function getSliderValue() 
+{
+    var rateval = document.getElementById("rate").value+"%";
+    document.getElementById("rate_val").innerText=rateval;
+}        
+
+function validateAmount() 
+{
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    return true;
+
+}
